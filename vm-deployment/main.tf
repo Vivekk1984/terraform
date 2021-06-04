6351644adf323f075a0185e66896bc77ee1ec0fc
Vivekk1984 <52989642+Vivekk1984@users.noreply.github.com>
2021-06-04 13:16:25 -0400

    changes

--- vm-deployment/main.tf
+++ vm-deployment/main.tf
@@ -10,8 +10,6 @@ terraform {
 
 provider "azurerm" {
   subscription_id = "7466e80e-8114-4392-95c4-6a6c8540d8fe"
+  tenant_id       = "189de737-c93a-4f5a-8b68-6f4ca9941912"
   features {}
-}
-provider "azuread" {
-  tenant_id = "189de737-c93a-4f5a-8b68-6f4ca9941912"
 }
